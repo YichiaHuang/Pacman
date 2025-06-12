0c953360b981252adcb114ba81c38bd638183f34
OwO1732 <abbytsai0913@gmail.com>
2025-06-12 12:51:44 +0800

    adjusted the setDir() and escape() condition to 8.0f. you can test which number has the best effect. ( up to 32.0, I guess )

--- Ghost/Ghost.cpp
+++ Ghost/Ghost.cpp
@@ -143,7 +143,7 @@ void Ghost::Update(float deltaTime) {
     float centerY = gridY * PlayScene::BlockSize + PlayScene::BlockSize / 2;
     float distToCenter = std::hypot(Position.x - centerX, Position.y - centerY);
 
-    if (distToCenter < 5.0f) {
+    if (distToCenter < 8.0f) {
         Position.x = centerX;
         Position.y = centerY;
 
