d1a31a5ce1a55820403c64263106b4dbea401065
Yichia <yichiahuang1@gmail.com>
2025-06-12 14:49:22 +0800

    Replay Button

--- Scene/LoseScene.cpp
+++ Scene/LoseScene.cpp
@@ -17,12 +17,18 @@ void LoseScene::Initialize() {
     int halfH = h / 2;
     AddNewObject(new Engine::Image("lose/GameOver.png", halfW, halfH, 600, 600, 0.5, 0.5));
     AddNewObject(new Engine::Label("You Lose :(", "prstartk.ttf", 48, halfW, halfH / 4 + 10, 255, 255, 255, 255, 0.5, 0.5));
-    Engine::ImageButton *btn;
-    btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW - 200, halfH * 7 / 4 - 50, 400, 100);
+    // Replay Button - 左邊
+    Engine::ImageButton* btn;
+    btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW - 250 - 200, halfH * 7 / 4 - 50, 400, 100);
+    btn->SetOnClickCallback(std::bind(&LoseScene::ReplayOnClick, this));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Replay", "prstartk.ttf", 48, halfW - 250, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
+
+    // Back Button - 右邊
+    btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW + 250 - 200, halfH * 7 / 4 - 50, 400, 100);
     btn->SetOnClickCallback(std::bind(&LoseScene::BackOnClick, this, 2));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Back", "prstartk.ttf", 48, halfW, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
-    //bgmInstance = AudioHelper::PlaySample("astronomia.ogg", false, AudioHelper::BGMVolume, PlayScene::DangerTime);
+    AddNewObject(new Engine::Label("Back", "prstartk.ttf", 48, halfW + 250, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
 }
 void LoseScene::Terminate() {
     //AudioHelper::StopSample(bgmInstance);
@@ -32,4 +38,8 @@ void LoseScene::Terminate() {
 void LoseScene::BackOnClick(int stage) {
     // Change to select scene.
     Engine::GameEngine::GetInstance().ChangeScene("stage-select");
+}
+
+void LoseScene::ReplayOnClick() {
+    Engine::GameEngine::GetInstance().ChangeScene("play");
 }
